4af49c8355c818d4d2825fbe2c572fd545d6d9ad
gcode101 <gcode101@users.noreply.github.com>
2022-11-08 23:23:24 -0500

    added css to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
 	return (
 		<header className="header">
 			<img src={trollface} className="trollface" alt='trollface'/>
-			
-
+			<h1 className="header--title">Meme Generator</h1>
+			<h4 className="header--project">React Course - Project 3</h4>
 		</header>
 	);
 }
 
-export default Header
+export default Header;
