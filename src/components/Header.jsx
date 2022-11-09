3231a1c2e297887ff318cf2a56d2184a30f82b97
gcode101 <gcode101@users.noreply.github.com>
2022-11-08 22:43:30 -0500

    added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import trollface from '../Trollface.webp';
+
+function Header() {
+	return (
+		<header className="header">
+			<img src={trollface} className="trollface" alt='trollface'/>
+			
+
+		</header>
+	);
+}
+
+export default Header
